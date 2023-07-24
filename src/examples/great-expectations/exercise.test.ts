aacb5d17723b0759df19ac8eac3b6d795f5bdf9d
Amalthean <amalthea.soft@gmail.com>
2023-07-24 13:41:37 +0200

    Add unit tests 'great-expectations'

--- src/examples/great-expectations/exercise.test.ts
+++ src/examples/great-expectations/exercise.test.ts
@@ -12,18 +12,19 @@ import { KanbanBoard } from '$lib/kanban-board';
  * toThrowError: https://vitest.dev/api/expect.html#tothrowerror
  */
 
-it.todo(
+it(
   'should pass if the two numbers would add up correctly in a language other than JavaScript',
   () => {
-    expect(0.2 + 0.1).toEqual(0.3);
+    expect(0.2 + 0.1).toBeCloseTo(0.3);
   },
 );
 
 describe('createPerson', () => {
-  it.todo('should create an instance of a person', () => {
+  it('should create an instance of a person', () => {
     const person = createPerson('Ada Lovelace');
     expect.hasAssertions();
     // Verify that person is an instance of a Person.
+    expect(person).toBeInstanceOf(Person);
   });
 });
 
@@ -34,64 +35,88 @@ describe('Kanban Board', () => {
     // Verify that board.statuses contains "Backlog".
   });
 
-  it.todo('should *not* include "Bogus" in board.statuses', () => {
+  it('should *not* include "Bogus" in board.statuses', () => {
     const board = new KanbanBoard('Things to Do');
     expect.hasAssertions();
+    expect(board.statuses).not.toContain('Bogus');
     // Verify that board.statuses does not contain "Bogus".
   });
 
-  it.todo(
+  it(
     'should include an added status in board.statuses using #addStatus',
     () => {
       const board = new KanbanBoard('Things to Do');
+      const status = 'my-status';
       expect.hasAssertions();
       // Use board.addStatus to add a status.
+      board.addStatus(status);
+      expect(board.statuses).toContain(status);
       // Verify that the new status is—in fact—now in board.statuses.
     },
   );
 
-  it.todo('should remove a status using #removeStatus', () => {
+  it('should remove a status using #removeStatus', () => {
     const board = new KanbanBoard('Things to Do');
+    const status = 'my-status';
     expect.hasAssertions();
     // Use board.removeStatus to remove a status.
-
+    board.addStatus(status);
+    expect(board.statuses).toContain(status);
     // You can be clever or you can just assume "Backlog" is in board.statuses
     // by default.
-
     // Verify that the status is no longer in in board.statuses.
+    board.removeStatus(status);
+    expect(board.statuses).not.toContain(status);
+    board.removeStatus('Backlog');
+    expect(board.statuses).not.toContain('Backlog');
   });
 });
 
 describe('Person', () => {
-  it.todo('will create a person with a first name', () => {
+  it('will create a person with a first name', () => {
     const person = new Person('Madonna');
     expect.hasAssertions();
     // Verify that person.firstName is correct.
+    expect(person.firstName).toBe('Madonna');
+    expect(person.middleName).toBeUndefined();
+    expect(person.lastName).toBeUndefined();
   });
 
-  it.todo('will create a person with a first and last name', () => {
+  it('will create a person with a first and last name', () => {
     const person = new Person('Madonna Cicone');
     expect.hasAssertions();
     // Verify that person.lastName is correct.
+    expect(person).toHaveProperty('firstName');
+    expect(person.firstName).toBe('Madonna');
+    expect(person).toHaveProperty('lastName');
+    expect(person.lastName).toBe('Cicone');
+    expect(person.middleName).toBeUndefined();
   });
 
-  it.todo('will create a person with a first, middle, and last name', () => {
+  it('will create a person with a first, middle, and last name', () => {
     const person = new Person('Madonna Louise Cicone');
     expect.hasAssertions();
     // Verify that person.middleName is correct.
+    expect(person).toHaveProperty('firstName');
+    expect(person.firstName).toBe('Madonna');
+    expect(person).toHaveProperty('middleName');
+    expect(person.middleName).toBe('Louise');
+    expect(person).toHaveProperty('lastName');
+    expect(person.lastName).toBe('Cicone');
+
   });
 
-  it.todo('will throw if you provide an empty string', () => {
+  it('will throw if you provide an empty string', () => {
     const fn = () => {
       new Person('');
     };
 
     expect.hasAssertions();
-
     // Verify that function above throws.
+    expect(fn).toThrow();
   });
 
-  it.todo(
+  it(
     'will throw a specific error message if you provide an empty string',
     () => {
       const errorMessage = 'fullName cannot be an empty string';
@@ -103,10 +128,11 @@ describe('Person', () => {
       expect.hasAssertions();
 
       // Verify that function above throws the error message above.
+      expect(fn).toThrowError(errorMessage);
     },
   );
 
-  it.todo('will add a friend', () => {
+  it('will add a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -115,9 +141,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that john.friends contains paul.
+    expect(john.friends).toContain(paul);
   });
 
-  it.todo('will mutually add a friend', () => {
+  it('will mutually add a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -126,9 +153,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that paul.friends contains john.
+    expect(paul.friends).toContain(john);
   });
 
-  it.todo('will remove a friend', () => {
+  it('will remove a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -138,9 +166,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that john.friends does not inclide paul.
+    expect(john.friends).not.contain(paul);
   });
 
-  it.todo('will mutually remove friends', () => {
+  it('will mutually remove friends', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -150,6 +179,7 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that paul.friends does not include john.
+    expect(paul.friends).not.contain(john);
   });
 });
 
@@ -158,11 +188,13 @@ const explode = () => {
 };
 
 describe('explode', () => {
-  it.todo('should throw an error', () => {
-    explode();
+  it('should throw an error', () => {
+    // explode();
+    expect(() => explode()).toThrow();
   });
 
-  it.todo('should throw a specific error containing "terribly wrong"', () => {
-    explode();
+  it('should throw a specific error containing "terribly wrong"', () => {
+    // explode();
+    expect(() => explode()).toThrowError('terribly wrong');
   });
 });

78227992381ba07a432f4e9af6f77adb68264417
Amalthean <amalthea.soft@gmail.com>
2023-07-24 13:43:26 +0200

    Add component tests 'counter'

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,26 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// @vitest-environment happy-dom
+import { screen, fireEvent } from '@testing-library/react';
+import { render } from './test/utilities';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+  screen.debug(document.body);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const counter = render(<Counter />);
+
+  const currentCount = screen.getByTestId('current-count');
+
+  // expect(currentCount.textContent).toBe('0');
+  expect(currentCount).toHaveTextContent('0');
+
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+
+  // fireEvent.click(incrementButton);
+  await counter.user.click(incrementButton);
+
+  // expect(currentCount.textContent).toBe('1');
+  expect(currentCount).toHaveTextContent('1');
+});

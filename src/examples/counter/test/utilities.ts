11422827efbb968014e9c98f9fcb51ff040f0af0
Amalthean <amalthea.soft@gmail.com>
2023-07-24 13:46:52 +0200

    Monkey patch render 'te@sting-library/react, setup import '@testing-library/jest-dom'

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,4 +1,18 @@
-export {};
+import type { ReactElement } from 'react';
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+type RenderOptions = Parameters<typeof renderComponent>[1];  // get the type of the 2nd parameter of renderComponent
+
+export * from '@testing-library/react';
+
+export const render = (ui: ReactElement, options?: RenderOptions) => {
+  return {
+    ...renderComponent(ui, options),
+    user: userEvent.setup(),
+  };
+};
+
 
 /**
  * For a complete example, see: test/utilities.ts
--- src/examples/counter/vitest.config.ts
+++ src/examples/counter/vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    setupFiles: path.resolve(__dirname, './test/setup.ts'),
     exclude: [...defaultExclude, '**/*.svelte**'],
   },
 });
